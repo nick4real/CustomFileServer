8a5f9318fbb492f50e00d4c934ecf5155850011a
Nikolaj Kurkul <67540024+nick4real@users.noreply.github.com>
2026-05-13 11:32:48 +0200

    refactor

--- CFS/CFS.Infrastructure/Persistence/AppDbContext.cs
+++ CFS/CFS.Infrastructure/Persistence/AppDbContext.cs
@@ -7,5 +7,5 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<Metadata> Metadatas { get; set; } = null!;
+    public DbSet<Metadata> Metadatas { get; set; }
 }
