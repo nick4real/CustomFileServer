19dfdbdbbc58343e1ef9b8598a4ff30beea8cb51
Nikolaj Kurkul <67540024+nick4real@users.noreply.github.com>
2026-05-13 16:05:03 +0200

    react project

---
+++ cfs.reactwebapp/src/components/ListGroup.tsx
@@ -0,0 +1,24 @@
+function ListGroup() {
+  const items = [
+    "An item",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
+  ];
+
+  return (
+    <>
+      <h1>List Group</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
